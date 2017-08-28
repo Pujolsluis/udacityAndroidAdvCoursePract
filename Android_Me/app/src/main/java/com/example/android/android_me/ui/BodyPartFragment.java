5c8784a08879f284f68219d85dc72bd66009b710
Luis Pujols <luispujols@hotmail.com>
2017-08-28 18:57:26 -0400

    REFACTOR 'implemented the new logic to dinamicly assign the imagelist to the BodyPartFragment and the index of the desired list to show'

--- Android_Me/app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ Android_Me/app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -3,6 +3,7 @@ package com.example.android.android_me.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +12,17 @@ import android.widget.ImageView;
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
 
+import java.util.List;
+
 /**
  * Created by Oficina on 28/08/2017.
  */
 
 public class BodyPartFragment extends Fragment {
+
+    private static final String TAG = BodyPartFragment.class.getSimpleName();
+    private List<Integer> mImageList;
+    private int mImageIndex;
 
     //Mandatory constructor to instantiating the fragment
     public BodyPartFragment(){
@@ -32,10 +39,29 @@ public class BodyPartFragment extends Fragment {
         //Get a reference to the imageview in the fragment layout
         ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
 
-        //Set the image resource to display
-        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
-
+        if(mImageList != null) {
+            //Set the image resource to display
+            imageView.setImageResource(AndroidImageAssets.getHeads().get(mImageIndex));
+        }else{
+            Log.d(TAG, "ImageList is null");
+        }
         return rootView;
 
+    }
+
+    public List<Integer> getmImageList() {
+        return mImageList;
+    }
+
+    public void setmImageList(List<Integer> mImageList) {
+        this.mImageList = mImageList;
+    }
+
+    public int getmImageIndex() {
+        return mImageIndex;
+    }
+
+    public void setmImageIndex(int mImageIndex) {
+        this.mImageIndex = mImageIndex;
     }
 }
